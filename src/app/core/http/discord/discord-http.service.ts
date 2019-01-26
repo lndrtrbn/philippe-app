a3d1783179520d450fa8def85ff7b425e112dee5
Landry Trebon <landry.trebon@gmail.com>
2019-01-26 22:51:23 +0100

    get data via http services on start

--- src/app/core/http/discord/discord-http.service.ts
+++ src/app/core/http/discord/discord-http.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { DiscordServer } from '@core/store/discord/discord.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiscordHttpService {
 
-  constructor() { }
+  constructor() {}
+
+  /**
+   * Retrieves the information about the discord server
+   * which Philippe is connected
+   * @returns The discord server infos
+   */
+  getDiscordServerInfos(): Observable<DiscordServer> {
+    // Should call an API there
+    return of({
+      id: 'blblbl',
+      name: 'Retards Society',
+      channels: [
+        { id: '1', name: 'Channel 1', people: [] },
+        { id: '2', name: 'Channel 2', people: [{ name: 'Jean Mich' }] },
+        { id: '3', name: 'Channel 3', people: [{ name: 'Jacques' }, { name: 'J0si4nn3' }] },
+        { id: '4', name: 'Channel 4', people: [{ name: 'DK v4d0R' }] }
+      ]
+    });
+  }
 }
--- src/app/core/http/user/user-http.service.ts
+++ src/app/core/http/user/user-http.service.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class UserHttpService {
 
-  constructor() { }
+  constructor() {}
 }
